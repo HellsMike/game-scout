07853a8ab4746f17e9aec440f60cde5e5e234c2c
Edoardo Pracucci <edoardo.pracucci@gmail.com>
2021-11-15 10:45:33 +0100

    Updating homepage
    update mystyle.css, change hex to RGB
    add log out in homepage.html
    update some file html with template extends

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,22 @@
+import logging
+
+from django.contrib import messages
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'customer/signup.html')
+    elif request.method == 'POST':
+        logging.warning('Email: ' + request.POST['email'])
+
+        if(request.POST['password'] != request.POST['password_confirmation']):
+            messages.error(request, 'username or password not correct')
+            return render(request, 'customer/signup.html')
+        try:
+            user = User.objects.create_user(request.POST['email'], request.POST['email'], request.POST['password'])
+            user.save()
+        except():
+            return render(request, 'customer/signup.html')
+    return render(request, 'accounts/login.html')
