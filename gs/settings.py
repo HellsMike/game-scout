2d4c96a9050d17282d13998ec203dc0520a0ad51
Michele Bianco <bianco.michele@outlook.com>
2021-12-31 12:52:15 +0100

    Add crispy-forms package + minor fixes
    Fix: null and invalid operation management on review view

--- gs/settings.py
+++ gs/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Custom apps
-    # 'ecommerce.apps.EcommerceConfig',
+    'ecommerce.apps.EcommerceConfig',
     'customer.apps.CustomerConfig',
     'review.apps.ReviewConfig',
-    'ecommerce',
-    'django_seed'
+    'django_seed',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- review/views.py
+++ review/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.http import Http404
 from django.shortcuts import render
 from django.template.defaulttags import register
@@ -5,6 +6,7 @@ from django.template.defaulttags import register
 # Create your views here.
 from ecommerce.models import Product
 from review.models import Review
+
 
 @register.filter
 def get_review(reviews, index):
@@ -17,14 +19,15 @@ def review(request):
         current_product = Product.objects.get(pk=product_id)
         current_reviews = Review.objects.filter(product_id=product_id).order_by('date')
         review_count = Review.objects.filter(product_id=product_id).count()
-        total_rate = Review.objects.filter(product_id=product_id).aggregate(Sum('rate'))
+        total_rate = Review.objects.filter(product_id=product_id).aggregate(Sum('rate'))["rate__sum"] or 0
+        product_rate = total_rate / review_count if review_count != 0 else 0
     except Product.DoesNotExist:
         raise Http404("The the product with the ID:" + product_id + " does not exist")
 
     context = {
         'reviews': current_reviews,
         'numberreview': review_count,
-        'product_rate': total_rate/review_conut,
+        'product_rate': product_rate,
         'product': current_product,
     }
-    return render(request,'review/review.html',context)
+    return render(request, 'review/review.html', context)
