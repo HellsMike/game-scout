cd4cbe39d5b700d8811e9ed9d7822191d77277d8
Michele Bianco <HellsMike>
2022-01-03 15:46:31 +0100

    Fix minor bug for review

--- review/forms.py
+++ review/forms.py
@@ -2,15 +2,7 @@ from django import forms
 from review.models import Review
 
 
-class ReviewCrispyForm(forms.ModelForm):
-    pass
-    """
-    helper = FormHelper()
-    helper.from_id = 'review-crispy-form'
-    helper.form_method = 'POST'
-    helper.add_input(Submit('submit', 'Submit'))
-
+class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ('title', 'text', 'rate')
-        """
--- review/views.py
+++ review/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.template.defaulttags import register
 from ecommerce.models import Product
 from .models import Review
-from .forms import ReviewCrispyForm
+from .forms import ReviewForm
 
 
 @register.filter
@@ -17,7 +17,7 @@ def review(request):
         product_id = request.POST.get('id')
         print(product_id)
         product = get_object_or_404(Product, pk=product_id)
-        form = ReviewCrispyForm(request.POST)
+        form = ReviewForm(request.POST)
         if form.is_valid():
             new_review = form.save(commit=False) # Don't save it yet
             new_review.product = product # Add product
@@ -31,7 +31,7 @@ def review(request):
             review_count = Review.objects.filter(product_id=product_id).count()
             total_rate = Review.objects.filter(product_id=product_id).aggregate(Sum('rate'))["rate__sum"] or 0
             product_rate = (total_rate / review_count) if review_count != 0 else 0
-            form = ReviewCrispyForm()
+            form = ReviewForm()
         except Product.DoesNotExist:
             return HttpResponseNotFound("The the product with the ID:" + product_id + " does not exist") 
     context = {
