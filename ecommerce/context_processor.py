473cc73037335c3ad7cd27af6c3b97ef25597d8f
Edoardo Pracucci <edoardo.pracucci@gmail.com>
2022-01-14 19:06:51 +0100

    Updating template to render cart and wishlist number

--- ecommerce/context_processor.py
+++ ecommerce/context_processor.py
@@ -1,9 +1,13 @@
-from ecommerce.models import Transaction
+from ecommerce.models import Transaction, Product
+from django.contrib.auth.models import User
 from customer.models import Wishlist
 
 def get_nav_count(request):
-    # user_transaction_count = Transaction.object.filter(customer=request.user)
-    # print(user_transaction_count)
-    mes="ciao"
+    user_transaction_count = Transaction.objects.filter(customer=request.user,state=Transaction.pending).count()
+    user_wishlist_count = (Product.objects.filter(wishlist__user=request.user).count())
 
-    return {'mes':mes}
+    return {
+        'transaction_count':user_transaction_count,
+        'wishlist_count':user_wishlist_count,
+
+            }
