dcbf420483e7b2e62700709eb81fc5756d5a3371
Edoardo Pracucci <edoardo.pracucci@gmail.com>
2022-01-14 18:40:49 +0100

    Updating template

--- ecommerce/context_processor.py
+++ ecommerce/context_processor.py
@@ -2,7 +2,8 @@ from ecommerce.models import Transaction
 from customer.models import Wishlist
 
 def get_nav_count(request):
-    user_transaction_count = Transaction.object
+    # user_transaction_count = Transaction.object.filter(customer=request.user)
+    # print(user_transaction_count)
     mes="ciao"
 
     return {'mes':mes}
