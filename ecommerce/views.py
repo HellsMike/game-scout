49cae21140c8112c9908461bd5c4933cc216340c
Edoardo Pracucci <edoardo.pracucci@gmail.com>
2022-02-06 10:10:04 +0100

    Updating keymanager

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -227,7 +227,7 @@ def catalog(request):
         if maxSaleSet["sale__max"] is not None:
             sales[product.id] = maxSaleSet['sale__max']
 
-    genres = Genre.objects.all()[:11]
+    genres = Genre.objects.all()
     context = {
         'results': page_results,
         'currentPage': page,
