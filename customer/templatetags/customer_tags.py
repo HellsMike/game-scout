05bba8607b2e378892eb4fba4578034bbde2c499
Michele Bianco <bianco.michele@outlook.com>
2022-04-11 16:29:28 +0200

    Moved filters to more appropriate file

---
+++ customer/templatetags/customer_tags.py
@@ -0,0 +1,28 @@
+from django import template
+from django.db.models import Sum
+from ecommerce.models import Key
+from review.models import Review
+
+
+register = template.Library()
+
+
+@register.filter
+def get_key_count(product):
+    return Key.objects.filter(product=product, sold=False).count()
+
+
+@register.filter
+def get_key_sale_count(product):
+    return Key.objects.filter(product=product, sold=False, sale__gt=0).count()
+
+
+@register.filter
+def get_max_sale(product):
+    sale = Key.objects.filter(product=product, sold=False, sale__gt=0).order_by('-sale').values('sale').first()
+    return sale['sale'] if sale!=None else 0
+
+
+@register.filter
+def get_key_insale_count(product):
+    return Key.objects.filter(product=product, sold=False, sale__gt=0).count()
