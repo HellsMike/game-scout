c3a4b37afa2f019c59a4d4a4f9597743fa9b4029
Michele Bianco <bianco.michele@outlook.com>
2022-04-11 16:29:43 +0200

    Add filters

---
+++ ecommerce/templatetags/ecommerce_tags.py
@@ -0,0 +1,57 @@
+from django import template
+from django.db.models import Sum
+from ecommerce.models import Key
+from review.models import Review
+
+
+register = template.Library()
+
+
+@register.filter
+def get_total_rate(product):
+    review_count = Review.objects.filter(product_id=product.id).count()
+    product_rate = 0
+
+    if review_count > 0:
+        total_rate = Review.objects.filter(product_id=product.id).aggregate(Sum('rate'))["rate__sum"] or 0
+        product_rate = (total_rate / review_count)
+
+    return product_rate
+
+
+@register.filter
+def get_rate_count(product):
+    return Review.objects.filter(product_id=product.id).count()
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+
+@register.filter
+def extract_price(keys, index):
+    return keys[index].price
+
+
+@register.filter
+def get_seller_data(user):
+    seller_sold_keys_count = Key.objects.filter(seller=user, sold=True).count()
+
+    return seller_sold_keys_count
+
+
+@register.filter
+def get_best_price(product):
+    if Key.objects.filter(product=product, sold=False).order_by('price').count()==0:
+        best_price="out of stock"
+    else:
+        key_best_price = Key.objects.filter(product=product, sold=False).order_by('price')
+        best_price = key_best_price[0].price
+
+    return best_price
+
+
+@register.filter
+def get_seller_rate(user):
+    return user.profile.seller_total_ratings/user.profile.seller_ratings_count if user.profile.seller_ratings_count>0 else "Nessusa valutazione"
