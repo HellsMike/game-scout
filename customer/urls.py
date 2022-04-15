368477a0268cba4bd19b3cd082a179c1aa7c064c
Michele Bianco <bianco.michele@outlook.com>
2022-04-15 14:44:25 +0200

    Fixed some urls in template for DRY standard

--- customer/urls.py
+++ customer/urls.py
@@ -4,22 +4,17 @@ from . import  views
 app_name = 'customer'
 
 urlpatterns = [
-    path('keymanager', views.keymanager, name=''),
-    path('library', views.library, name=''),
+    path('keymanager', views.keymanager, name='keymanager'),
+    path('library', views.library, name='library'),
     path('settings', views.profilesettings, name='settings'),
     path('signup', views.signup, name='signup'),
-    path('wishlist', views.wishlist, name=''),
+    path('wishlist', views.wishlist, name='wishlist'),
     path('become-seller', views.becomeseller, name='become-seller'),
     path('become-customer', views.becomecustomer, name='become-customer'),
     path('add-to-wishlist', views.add_to_wishlist, name='add-to-wishlist'),
     path('remove-from-wishlist', views.remove_from_wishlist, name='remove-from-wishlist'),
     path('change-pro-pic', views.change_pro_pic, name='change-pro-pic'),
-
     path('add-key', views.add_key, name='add-key'),
     path('delete-key', views.delete_key_by_seller, name='delete-key'),
     path('modify-key', views.modify_key, name='modify-key'),
-
-    #path('add-key-confirmation', views.addkeyconfirmation, name='add-key-confirmation'),
-    # path('user', views.user, name='user'),
-    # path('provaform', views.provaform, name=''),
 ]
