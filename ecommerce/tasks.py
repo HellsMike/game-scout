dccd1894eb5b5759ece7960f2c7f3df431ae5288
Michele Bianco <bianco.michele@outlook.com>
2022-04-28 14:39:34 +0200

    Add background task to remove product from cart

--- ecommerce/tasks.py
+++ ecommerce/tasks.py
@@ -1,10 +1,9 @@
-#from background_task import background
+from background_task import background
 from ecommerce.models import Transaction
 
 
-#@background(schedule=3600)
+@background(schedule=1800)
 def t_remove_from_cart(trans_id):
-    #trans_to_remove = Transaction.objects.filter(id=trans_id)
-    #if trans_to_remove.state == Transaction.pending:
-    #    trans_to_remove.delete()
-    pass
+    trans_to_remove = Transaction.objects.filter(id=trans_id).first()
+    if trans_to_remove.state == Transaction.pending:
+        trans_to_remove.delete()
