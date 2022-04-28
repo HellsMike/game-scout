824ce17a0bed1d8c99c8046d78415dd27436698d
Michele Bianco <bianco.michele@outlook.com>
2022-04-28 15:00:14 +0200

    Fix for cleaner terminal

--- ecommerce/tasks.py
+++ ecommerce/tasks.py
@@ -5,5 +5,7 @@ from ecommerce.models import Transaction
 @background(schedule=1800)
 def t_remove_from_cart(trans_id):
     trans_to_remove = Transaction.objects.filter(id=trans_id).first()
-    if trans_to_remove.state == Transaction.pending:
-        trans_to_remove.delete()
+    if trans_to_remove:
+        if trans_to_remove.state == Transaction.pending:
+            trans_to_remove.delete()
+            print(f'Transacion #{trans_id} removed')
